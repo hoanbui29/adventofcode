c8d3e39591420277e6742ea4674e26812ea2228f
hoanbui29 <81216107+hoanbui29@users.noreply.github.com>
2024-12-05 09:12:06 +0700

    Update Rust solution day 3

--- 2024/day3/rust/src/main.rs
+++ 2024/day3/rust/src/main.rs
@@ -1,4 +1,4 @@
-use fancy_regex::Regex;
+use regex::Regex;
 
 fn get_input() -> String {
     std::fs::read_to_string("input.txt").unwrap()
@@ -7,7 +7,7 @@ fn get_input() -> String {
 fn part_one(input: &str) {
     let r = Regex::new(r"mul\(\d+,\d+\)").unwrap();
     let mut total = 0;
-    for m in r.find_iter(&input).map(|m| m.unwrap().as_str()) {
+    for m in r.find_iter(&input).map(|m| m.as_str()) {
         total += m[4..m.len() - 1]
             .split(",")
             .map(|n| n.parse::<i32>().unwrap())
@@ -18,17 +18,22 @@ fn part_one(input: &str) {
 }
 
 fn part_two(input: &str) {
-    let enabled_groups = Regex::new(r"(?<=do\(\)|\A)[\s\S]*?(?=don\'t\(\)|\z)").unwrap();
+    let r = Regex::new(r"mul\((\d+),(\d+)\)|(d)(o)(?:n't)?\(\)").unwrap();
+
+    let mut enabled = true;
     let mut total = 0;
-    let enabled_groups = enabled_groups.find_iter(&input);
-    for eg in enabled_groups {
-        let r = Regex::new(r"mul\(\d+,\d+\)").unwrap();
-        for cap in r.find_iter(eg.unwrap().as_str()).map(|m| m.unwrap()) {
-            let op = cap.as_str();
-            total += op[4..op.len() - 1]
-                .split(",")
-                .map(|n| n.parse::<i32>().unwrap())
-                .product::<i32>();
+
+    for (m, [x, y]) in r.captures_iter(input).map(|m| m.extract()) {
+        match m {
+            "do()" => enabled = true,
+            "don't()" => enabled = false,
+            _ => {
+                total += if enabled {
+                    x.parse::<i32>().unwrap() * y.parse::<i32>().unwrap()
+                } else {
+                    0
+                };
+            }
         }
     }
 
